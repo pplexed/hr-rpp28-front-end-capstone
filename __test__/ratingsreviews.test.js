cda34a1a922aaf64bf359c6795cdeead5a93f3df
Chris Floyd <chris.floyd.mail@gmail.com>
2021-06-03 19:38:06 -0700

    Added CSS, Folder for Reviews and components

--- __test__/ratingsreviews.test.js
+++ __test__/ratingsreviews.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import RatingsReviews from '../client/src/components/ratingsreviews';
+import RatingsReviews from '../client/src/components/reviews/ratingsreviews';
 
 test('Text inside Ratingsreviews', () => {
- 
+
   const wrapper = shallow(<RatingsReviews/>);
 
-  expect(wrapper.text()).toEqual('This is the Ratings Reviews Module');
+  expect(wrapper.text()).toEqual('sort will go here<IRT />MORE REVIEWS');
 
 });
