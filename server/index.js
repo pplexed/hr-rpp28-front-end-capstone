3c46c1772e5629bfe9ac49ada1e00bfa4b04c535
Chris Floyd <chris.floyd.mail@gmail.com>
2021-06-05 13:22:23 -0700

    Completed review routes in server

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes.js');
 const review = require('./routes/reviewroutes.js')
-
-
+var QAroutes = require('./routes/QA_routes.js');
 
 const PORT = 3000;
 
@@ -12,15 +11,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('./client/dist'));
 
-app.get('/reviews', (req, res) => {
-  review.getReviews((err, data) => {
-    console.log('we are here');
-    if (err) {
-      console.log(err);
-    } else {
 //import routes
-var QAroutes = require('./routes/QA_routes.js');
 app.use('/qa', QAroutes);
+app.use('/reviews', review);
+
+// app.get('/reviews', (req, res) => {
+//   review.getReviews((err, data) => {
+//     console.log('we are here');
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(data);
+//     }
+//   });
+// });
+
+
 
 
 app.get('/', (req, res) => {
@@ -28,9 +34,7 @@ app.get('/', (req, res) => {
   //res.send('basic get request received');
   console.log('received a request at /');
 
-      res.send(data);
-    }
-  })
+  res.send(data);
 });
 
 
--- server/routes/reviewroutes.js
+++ server/routes/reviewroutes.js
@@ -1,7 +1,16 @@
-
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
 const axios = require('axios');
 const token = require('../../config.js')
 const url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews?product_id=22125'
+
+// default options as middleware
+router.use(fileUpload());
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
 
 const options = {
   method: 'get',
@@ -9,13 +18,25 @@ const options = {
   headers: token.auth
 }
 
+router.get('/product', (req, res) => {
+  console.log('working');
+  getReviews((err, data) => {
+    console.log('we are here');
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
 const getReviews = (callback) => {
 
   axios(options)
     .then((response) => {
-      callback(response.data);
+      callback(null, response.data);
     });
-
 };
 
-module.exports.getReviews = getReviews;
+// module.exports.getReviews = getReviews;
+module.exports = router;
