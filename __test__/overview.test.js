e2b5149d95ddc2f7d6d501de1ee162ef7ccaeeac
Rob Stafford <uwillgo2themoon@gmail.com>
2021-06-07 10:00:14 -0700

    updated test to point at correct overview page

--- __test__/overview.test.js
+++ __test__/overview.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import Overview from '../client/src/components/overview';
+import Overview from '../client/src/components/overview/overview.jsx';
 
 test('Text inside Overview', () => {
- 
+
   const wrapper = shallow(<Overview />);
 
   expect(wrapper.text()).toEqual('This is the overview module');
