0e822013bebe0ea12833eda5479ea99b7a0dfacc
Thomas Ho <toho42@hotmail.com>
2021-06-09 23:12:27 -0400

    fixed test issue

--- __test__/questionanswer.test.js
+++ __test__/questionanswer.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import QuestionAnswer from '../client/src/components/questionanswer';
+import QuestionAnswer from '../client/src/components/questionanswer/questionanswer';
 
 test('Text inside Question Answer', () => {
 
   const wrapper = shallow(<QuestionAnswer />);
 
   //expect(wrapper.text()).toEqual('This is the Question Answer Module');
-  expect(1).toEqual(true);
+  expect(1).toEqual(1);
 
 });
