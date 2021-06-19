5e53c72156a17caadf1fce7f5d76a519be5e8e62
Tim Moses <trmoses42@gmail.com>
2021-06-19 10:06:17 -0500

    Removed old files and unused script

--- server/routes/relatedItems_routes.js
+++ server/routes/relatedItems_routes.js
@@ -56,52 +56,5 @@ router.get('/products', (req, res) => {
     }
   });
 });
-// router.get('/relatedItems', (req, res) => {
-//   // Turn this line on when app goes live
-//   // const builtUrl = url + `/products/${req.data.product_id}/related`;
-
-//   // Turn this line off when app goes live
-//   var itemData = {};
-//   const builtUrl = url + `/products/${product_id}/related`;
-
-//   const options = {
-//     method: 'get',
-//     url: builtUrl,
-//     headers: token.AUTH,
-//   };
-//   axios.defaults.headers.common['Authorization'] = token.TOKEN;
-
-//   axios(options)
-//     .then(({ data }) => {
-//       return (
-//         Promise.all(data.map((id) => {
-//           return axios(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${id}`)
-//             .then(({ data }) => {
-//               return itemData[data.id] = data;
-//             });
-//         }))
-//       );
-//     }).then((itemData) => {
-//       res.send(itemData);
-//     }).catch((err) => {
-//       console.log(err);
-//     });
-// });
-
-// router.get(`/relatedItems/products/:product_id/styles`, (req, res) => {
-//   const options = {
-//     method: 'get',
-//     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${product_id}/styles`,
-//     headers: token.AUTH,
-//   };
-
-//   axios(options)
-//     .then( (data) => {
-//       res.send(data);
-//     })
-//     .catch((err) => {
-//       console.log('Error fetching styles data: ', err);
-//     });
-// });
 
 module.exports = router;
