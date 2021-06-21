4e6ff264b66da2f15bf682ff3dfe184f00c30873
Thomas Ho <toho42@hotmail.com>
2021-06-21 16:27:59 -0400

    fixing tests to pass CI (no direct call to axios made)

--- __test__/QuestionAnswer-Test/questionanswer.test.js
+++ __test__/QuestionAnswer-Test/questionanswer.test.js
@@ -4,10 +4,8 @@ import QuestionAnswer from '../../client/src/components/questionanswer/questiona
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { act } from "react-dom/test-utils";
-//import { render, Simulate, flushPromises } from 'react-testing-library';
 import listQuestions from './listquestionsexample.js';
 import axios from 'axios';
-import { assertTSExpressionWithTypeArguments } from '@babel/types';
 import { debug } from 'webpack';
 //import userEvent from '@testing-library/user-event'
 //jest.mock('axios');
@@ -36,10 +34,6 @@ describe('Unit Test Section: <QuestionAnswer/>', () => {
     expect(1).toEqual(1);
   });
 
-
-  test('Unit Test 1:  Does the component <QuestionAnswer/> render?', () => {
-    render(<QuestionAnswer currentProduct={secondProduct}/>);
-  });
 
   test('Unit Test 1:  Does the component <QuestionAnswer/> render?', () => {
     render(<QuestionAnswer currentProduct={secondProduct}/>);
@@ -83,10 +77,12 @@ describe('Integration Test: : <QuestionAnswer/>', () => {
   });
 
 
-  test('dummy integration test', () => {});
+  test('dummy integration test', () => {
+    expect(1).toEqual(1);
+  });
 
 
-  // PASSING!  deactivated because it requires jest.mock('axios) which breaks other tests.
+  // PASSING!  deactivated because it requires jest.mock('axios') which breaks other tests.
   // test('first integration test', async () => {
   //   act(() => {
   //     render(<QuestionAnswer currentProduct={secondProduct}/>, container);
@@ -100,15 +96,12 @@ describe('Integration Test: : <QuestionAnswer/>', () => {
   //     //await waitFor(() => expect(screen.getByText('answer')).toBeInTheDocument()); 
   //   })
   // });
-    
   // NOW PASSING!
 
 
 
   /* UNCOMMENT HERE
-
   test('second integration test without mocking API call results', async () => {
-
     await waitFor(() => {   
       //fixed bug of not rendering because of CORS
       var answer = screen.getAllByText('| add answer');
@@ -124,15 +117,11 @@ describe('Integration Test: : <QuestionAnswer/>', () => {
       expect(submitOfAnswerModalWindow).toBeInTheDocument();
     }); 
   });
-
   **UNCOMMENT HERE*/
 
 
   // NOW PASSING!
-
-
   /**** UNCOMMENT HERE
-   
   test('3rd Integration Test:  User enters fields, but email format improper, and clicks submit', async () => {
     await waitFor(() => { 
 
@@ -184,13 +173,7 @@ describe('Integration Test: : <QuestionAnswer/>', () => {
 
 
 
-
-
-
-
-
-
-  // NOW PASSING!  --NO LONGER PASSING
+  // --NO LONGER PASSING
   // test('4th Integration Test:  User clicks to Report and the Link changes to Reported', async () => {
   //   await waitFor(() => { 
 
@@ -204,103 +187,3 @@ describe('Integration Test: : <QuestionAnswer/>', () => {
 
 });
 
-
-
-
-
-//   test('Test #1: Does it render a Question & Answer Widget', () => {
-//    //screen.debug();
-//    //render(<QuestionAnswer currentProduct={secondProduct}/>);
-//    expect(screen.getByText('Search Question')).toBeInTheDocument();  
-//   });
-
-
-
-//     // describe('fetchData', () => {
-//     //  test('fetches successfully data from an API', async () => {
-//     //     const data = {
-//     //       data: {
-//     //         hits: [
-//     //           {
-//     //             objectID: '1',
-//     //             title: 'a',
-//     //           },
-//     //           {
-//     //             objectID: '2',
-//     //             title: 'b',
-//     //           },
-//     //         ],
-//     //       },
-//     //     };
-    
-//     //     axios.get.mockImplementationOnce(() => Promise.resolve(data));
-//     //   });
-    
-//     //   test('fetches erroneously data from an API', async () => {
-//     //     const errorMessage = 'Network Error';
-    
-//     //     axios.get.mockImplementationOnce(() =>
-//     //       Promise.reject(new Error(errorMessage)),
-//     //     );
-//     //   });
-//     // });
-
-  
-//   test('Test #2: It mocks an API call with test data from axios', () => {
-//     var textToSearchFor = 'Submit Photo';
-//     let inputTextBox = screen.getByRole('textbox');
-//     expect(screen.getByDisplayValue('Have a question? Search for answers…')).toBeInTheDocument(); 
-//     expect(inputTextBox).toBeInTheDocument();
-//    });
-
-
-
-//    test('test #3: make sure the add answer link is rendering', () => {
-  
-//     // The findBy search variant is used for asynchronous elements which will be there eventually. 
-//     // Tom Notes:  In my test, the 'add answer' "link" is not initially rendered, and therefore 
-//     // expect(screen.getByText('add answer')).toBeInTheDocument(); should fail
-//     // in my code once, the GET request to the API is returned &
-//     // once the state is populated, only then should my text render
-
-//     return screen.findByText(/add answer/).then(data => {
-//       expect(data).toBeInTheDocument(); 
-//     });
-//   });
-
-
-//    test('test #4: ', async () => {
-
-//     // note to self debug this later by getting the add answer field to load
-//     //let addAnswerLink = await screen.findByText('add answer');
-//     // userEvent.click(addAnswerLink);
-//     // expect(screen.getByDisplayValue('Example: jack543!')).toBeInTheDocument(); 
-//    });
-
-//   test('test #5: ', () => {
-//     //  expect(screen.getByDisplayValue('Example: jack543!')).toBeInTheDocument(); 
-//   });
-
-
-// });
-
-
-// test('should show the search bar with the basic heading Have a question? Search for answers…', () => {
-//   render(<QuestionAnswer />)
-//   const input = screen.getByLabelText('Have a question? Search for answers…');
-//   // Events and assertions...
-
-//   expect(input).toEqual(true);
-// })
-
-
-// const Button = ({ onClick, children }) => (
-//   <button onClick={onClick}>{children}</button>
-// )
-
-// test('calls onClick prop when clicked', () => {
-//   const handleClick = jest.fn()
-//   render(<Button onClick={handleClick}>Click Me</Button>)
-//   fireEvent.click(screen.getByText(/click me/i))
-//   expect(handleClick).toHaveBeenCalledTimes(1)
-// })
