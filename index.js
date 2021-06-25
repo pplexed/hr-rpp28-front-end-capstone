9dbc940adcdabaf177787bdcbfdfdbe88e075ec9
Chris Floyd <chris.floyd.mail@gmail.com>
2021-06-24 23:15:40 -0700

    Added stuff

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const RIroutes = require('./routes/relatedItems_routes.js');
-const review = require('./routes/reviewroutes.js')
-const QAroutes = require('./routes/QA_routes.js');
+const path = require('path');
+const RIroutes = require('./server/routes/relatedItems_routes.js');
+const review = require('./server/routes/reviewroutes.js')
+const QAroutes = require('./server/routes/QA_routes.js');
 
 const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static('./client/dist'));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // import routes
 app.use('/qa', QAroutes);
 app.use('/reviews', review);
 app.use('/relatedItems', RIroutes);
 
-app.get('/', (req, res) => {
-  console.log('this is data: ', req);
-  res.sendFile('./client/dist');
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
